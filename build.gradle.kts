15456a10ab1251ad428ffa206a13991f2c7a0cf4
Sett17 <raik.rohde@gmail.com>
2022-06-20 21:57:54 +0200

    Reading for publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,53 +1,70 @@
 plugins {
-    kotlin("multiplatform") version "1.7.0"
+  kotlin("multiplatform") version "1.7.0"
+  `maven-publish`
 }
 
 group = "de.okedikka"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 kotlin {
-    jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = "1.8"
-        }
-        withJava()
-        testRuns["test"].executionTask.configure {
-            useJUnitPlatform()
-        }
+  jvm {
+    compilations.all {
+      kotlinOptions.jvmTarget = "1.8"
     }
-    js(BOTH) {
-        browser {
-            commonWebpackConfig {
-                cssSupport.enabled = true
-            }
+    withJava()
+    testRuns["test"].executionTask.configure {
+      useJUnitPlatform()
+    }
+  }
+  js(BOTH) {
+    browser {
+      commonWebpackConfig {
+        cssSupport.enabled = true
+      }
+      testTask {
+        useKarma {
+          useChrome()
         }
+      }
     }
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
+  }
+  val hostOs = System.getProperty("os.name")
+  val isMingwX64 = hostOs.startsWith("Windows")
+  val nativeTarget = when {
+    hostOs == "Mac OS X" -> macosX64("native")
+    hostOs == "Linux"    -> linuxX64("native")
+    isMingwX64           -> mingwX64("native")
+    else                 -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+  }
 
-    
-    sourceSets {
-        val commonMain by getting
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-            }
-        }
-        val jvmMain by getting
-        val jvmTest by getting
-        val jsMain by getting
-        val jsTest by getting
-        val nativeMain by getting
-        val nativeTest by getting
+  sourceSets {
+    val commonMain by getting
+    val commonTest by getting {
+      dependencies {
+        implementation(kotlin("test"))
+      }
     }
+    val jvmMain by getting
+    val jvmTest by getting
+    val jsMain by getting
+    val jsTest by getting
+    val nativeMain by getting
+    val nativeTest by getting
+  }
+}
+
+publishing {
+  publications {
+    create<MavenPublication>("maven") {
+      groupId = "de.okedikka"
+      artifactId = rootProject.name
+      version = version
+
+      from(components["kotlin"])
+    }
+  }
 }
